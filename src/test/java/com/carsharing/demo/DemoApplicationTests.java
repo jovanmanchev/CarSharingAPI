e24c65b66586e40927f7a0dc3f15118d7a3ec27e
jovanmanchev <jovanmanchev3003@gmail.com>
2024-03-22 21:52:39 +0100

    initial prject commit

---
+++ src/test/java/com/carsharing/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsharing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
